ff4a9bfef90824083b06da94277203d6b3cb375a
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-05-31 15:25:15 -0400

    feat,init(all app) config vite - cleaning App.vue and add some variable - install package like chart.js

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, shallowRef, computed, watch, nextTick } from 'vue';
+import Chart from 'chart.js/auto';
+
+const weights = ref([])
+
+const weightChartEl = ref(null)
+
+const weightChart = shallowRef(null)
+
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style scoped>
+
+</style>
